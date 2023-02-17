7abaf1e9675f7356f04c4828811c8b9fb8783cc2
Khiro Sasha <sasha.khiro@gmail.com>
2023-02-17 14:30:56 +0200

    aded ID for ProductService in Client side project

---
+++ Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,16 @@
+﻿
+using BlazorCRUD.Shared;
+
+namespace BlazorCRUD.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+
+        Task GetProducts();
+        Task<Product?> GetProductById(int Id);
+        Task CreateProduct(Product product);
+        Task UpdateProduct(int id, Product product);
+        Task DeleteProduct(int id);
+    }
+}
---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorCRUD.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorCRUD.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetProductById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
